4a9aea05980b5a1cc34a5c3f19598c1a2c7e6d60
sirimri13 <lamquynhhuogn1304@gmail.com>
2020-06-17 12:49:11 +0700

    empty

---
+++ MyBoss-Admin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyBoss-Admin
+//
+//  Created by Huong Lam on 6/17/20.
+//  Copyright © 2020 Huong Lam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
