f962f6305635a347e59301ba5fd9ee49e2fcc8db
Minh <minh@Minhs-Mac.local>
2020-07-07 07:20:54 -0700

    Add Utilities

---
+++ MyBoss-Admin/Utilities.swift
@@ -0,0 +1,53 @@
+//
+//  Utilities.swift
+//  ClubManager
+//
+//  Created by Lam Huong on 12/10/19.
+//  Copyright © 2019 Lam Huong. All rights reserved.
+//
+import Foundation
+import UIKit
+
+class Utilities {
+    
+    static func styleTextField(_ textfield:UITextField) {
+        
+        // Create the bottom line
+        let bottomLine = CALayer()
+        
+        bottomLine.frame = CGRect(x: 0, y: textfield.frame.height - 2, width: textfield.frame.width, height: 2)
+        
+        bottomLine.backgroundColor = #colorLiteral(red: 0.07912478596, green: 0.09655424207, blue: 0.4037392437, alpha: 1).cgColor
+        
+        // Remove border on text field
+        textfield.borderStyle = .none
+        
+        // Add the line to the text field
+        textfield.layer.addSublayer(bottomLine)
+        
+    }
+    
+    static func styleFilledButton(_ button:UIButton) {
+        
+        // Filled rounded corner style
+        button.backgroundColor = #colorLiteral(red: 0.07912478596, green: 0.09655424207, blue: 0.4037392437, alpha: 1)
+        button.layer.cornerRadius = 25.0
+        button.tintColor = UIColor.white
+    }
+    
+    static func styleHollowButton(_ button:UIButton) {
+        
+        // Hollow rounded corner style
+        button.layer.borderWidth = 2
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.cornerRadius = 25.0
+        button.tintColor = UIColor.black
+    }
+    
+    static func isPasswordValid(_ password : String) -> Bool {
+        
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
+        return passwordTest.evaluate(with: password)
+    }
+    
+}
