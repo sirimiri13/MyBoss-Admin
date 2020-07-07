d0ec300160068e71a147ae70ca954e4de68f31f0
sirimri13 <lamquynhhuogn1304@gmail.com>
2020-07-07 10:51:44 +0700

    fix tableviewstaff

---
+++ MyBoss-Admin/ScanQRViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ScanQRViewController.swift
+//  MyBoss-Admin
+//
+//  Created by Huong Lam on 07/01/2020.
+//  Copyright © 2020 Huong Lam. All rights reserved.
+//
+
+import UIKit
+
+class ScanQRViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
